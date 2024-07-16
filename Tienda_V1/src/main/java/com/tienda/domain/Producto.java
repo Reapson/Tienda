f5d5e37ec58f9b8f79b19348605e3e76c0037d43
Ripichip <josueanticona@gmail.com>
2024-07-15 21:44:08 -0600

    Clase 9

--- Tienda_V1/src/main/java/com/tienda/domain/Producto.java
+++ Tienda_V1/src/main/java/com/tienda/domain/Producto.java
@@ -6,34 +6,37 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 import lombok.Data;
 
 @Data //Especificar que es una capa de datos
 @Entity //Esta clase esta enlazada a una tabla en la base de datos
-@Table(name="categoria") //Especificar cual entidad 
+@Table(name="producto") //Especificar cual entidad 
 
-public class Categoria implements Serializable{
+public class Producto implements Serializable{
     
     private static final long serialVersionUID = 1L; //Autoincremento en java
     
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name="id_producto")
     
-    private Long idCategoria; //El long es para que pueda ser mas largo el numero
+    private Long idProducto; //El long es para que pueda ser mas largo el numero
+    private String detalle;
+    private int existencias;
+    private double precio;
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
     
-    @OneToMany
-    @JoinColumn(name = "id_categoria", updatable=false)
-    List<Producto> productos;
+    //private long idCategoria;
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    Categoria categoria;
     
-    public Categoria() {
+    public Producto() {
     }
-    public Categoria(String categoria, boolean activo) {
-        this.descripcion = categoria;
+    public Producto(String producto, boolean activo) {
+        this.descripcion = producto;
         this.activo = activo;
     }
 }
